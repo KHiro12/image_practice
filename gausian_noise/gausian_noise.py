80eaf3fe2a013784774aad0dc85b23884dad3e8e
KHiro12 <0hf095277264f6e@ezweb.ne.jp>
2021-12-02 08:06:15 +0900

    Update gausian noise input type

--- gausian_noise/gausian_noise.py
+++ gausian_noise/gausian_noise.py
@@ -3,17 +3,17 @@ import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-def gausiannoise(img, sig, ave, file_no):
+def gausiannoise(img, sig, ave):
 	# np.random.normal(平均, 標準偏差, サイズ)
 	noise = np.random.normal(ave, sig, np.shape(img))
 	imgnoi = img + np.floor(noise)
 	imgnoi[imgnoi>255] = 255
 	imgnoi[imgnoi<0] = 0
 	# ノイズ付与画像
-	file_name = f'output/{str(file_no).zfill(2)}_gausunoise_{str(sig)}_{str(ave)}.jpg'
+	file_name = f'output/{str(1).zfill(2)}_gausunoise_{str(sig)}_{str(ave)}.jpg'
 	cv2.imwrite(file_name, imgnoi)
 	# ノイズ画像
-	file_name = f'output/{str(file_no).zfill(2)}_noise_{str(sig)}_{str(ave)}.jpg'
+	file_name = f'output/{str(1).zfill(2)}_noise_{str(sig)}_{str(ave)}.jpg'
 	cv2.imwrite(file_name, np.floor(noise))
 
 
@@ -23,16 +23,6 @@ if __name__ == "__main__":
 	cv2.imwrite('output/00_base.jpg', img)
 
 	# ガウシアンノイズ
-	for n in range(1, 10):
-		# 標準偏差
-		sig = 20 * n
-		ave = 0
-		gausiannoise(img, sig, ave, 1)
-
-		sig = 20
-		ave = 0 + n * 10
-		gausiannoise(img, sig, ave, 2)
-
-		sig = 20
-		ave = 0 - n * 10
-		gausiannoise(img, sig, ave, 2)
+	sig = int(input("標準偏差："))
+	ave = int(input("平均値："))
+	gausiannoise(img, sig, ave)
